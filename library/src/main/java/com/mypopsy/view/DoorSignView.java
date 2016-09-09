3a09e3c6ee229a8c2749e06741e0ff6cbf5b1676
Cerrato Renaud <renaud.cerrato@gmail.com>
2016-09-09 14:56:11 -0400

    initial import

---
+++ library/src/main/java/com/mypopsy/view/DoorSignView.java
@@ -0,0 +1,323 @@
+package com.mypopsy.view;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.v4.graphics.ColorUtils;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import com.mypopsy.doorsignview.R;
+
+/**
+ * Created by Cerrato Renaud <https://www.github.com/renaudcerrato>
+ */
+public class DoorSignView extends View {
+
+    public static final float SHADOW_DARK_RATIO = 0.7f;
+
+    private TextPaint mTextPaint;
+    private Paint mSignPaint;
+    private Paint mSignShadowPaint;
+    private Paint mStringsPaint;
+    private Paint mPinPaint;
+    private Paint mPinShadowPaint;
+
+    private Layout mTextLayout;
+
+    private CharSequence mText;
+    private int mCornerRadius;
+    private int mPinRadius;
+    private int mShadowSize;
+    private int mTextPaddingLeft;
+    private int mTextPaddingTop;
+    private int mTextPaddingRight;
+    private int mTextPaddingBottom;
+    private float mPinOffsetX, mPinOffsetY;
+
+    private RectF mBodyBounds = new RectF();
+    private Path mBodyPath;
+    private int mShadowOffsetX, mShadowOffsetY;
+
+
+    public DoorSignView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DoorSignView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, R.style.DefaultDoorScreenViewStyle);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public DoorSignView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        final Options args = Options.from(context, attrs, defStyleAttr, defStyleRes);
+
+        mTextPaint = new TextPaint();
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, args.textSize);
+        setTextColor(args.textColor);
+        setTypeface(args.typeFace);
+
+        mSignPaint = new Paint();
+        setSignColor(args.color);
+
+        mSignShadowPaint = new Paint();
+        mSignShadowPaint.setColor(darkerOf(args.color, SHADOW_DARK_RATIO));
+
+        mPinPaint = new Paint();
+        setPinColor(args.pinColor);
+
+        mPinShadowPaint = new Paint();
+        mPinShadowPaint.setColor(darkerOf(args.pinColor, SHADOW_DARK_RATIO));
+
+        mStringsPaint = new Paint();
+        setStringsColor(args.stringsColor);
+        setStringsWidth(args.stringsSize);
+
+        setText(args.text);
+        setCornerRadius(args.cornerRadius);
+        setPinRadius(args.pinRadius);
+        setShadowSize(args.shadowSize);
+        setTextPadding(args.textPaddingLeft, args.textPaddingTop, args.textPaddingRight, args.textPaddingBottom);
+        setPinOffset(args.pinOffsetX, args.pinOffsetY);
+        setLightAngle(-90);
+    }
+
+    private void setPinOffset(float pinOffsetX, float pinOffsetY) {
+        mPinOffsetX = pinOffsetX;
+        mPinOffsetY = pinOffsetY;
+        requestLayout();
+    }
+
+    public void setTextPadding(int left, int top, int right, int bottom) {
+        mTextPaddingLeft = left;
+        mTextPaddingTop = top;
+        mTextPaddingRight = right;
+        mTextPaddingBottom = bottom;
+        requestLayout();
+    }
+    
+    public void setShadowSize(int size) {
+        this.mShadowSize = size;
+        requestLayout();
+    }
+
+    public void setPinRadius(int radius) {
+        this.mPinRadius = radius;
+        requestLayout();
+    }
+
+    public void setCornerRadius(int radius) {
+        this.mCornerRadius = radius;
+        requestLayout();
+    }
+
+    public void setText(CharSequence charSequence) {
+        mText = charSequence == null ? "" : charSequence;
+        requestLayout();
+    }
+
+    public void setStringsWidth(int width) {
+        mStringsPaint.setStrokeWidth(width);
+        requestLayout();
+    }
+
+    public void setStringsColor(int color) {
+        mStringsPaint.setColor(color);
+        requestLayout();
+    }
+
+    public void setPinColor(int color) {
+        mPinPaint.setColor(color);
+        requestLayout();
+    }
+
+    public void setSignColor(int color) {
+        mSignPaint.setColor(color);
+        requestLayout();
+    }
+
+    public void setTypeface(Typeface typeFace) {
+        mTextPaint.setTypeface(typeFace);
+        requestLayout();
+    }
+
+    public void setTextSize(int unit, float size) {
+        mTextPaint.setTextSize(TypedValue.applyDimension(unit, size, getResources().getDisplayMetrics()));
+        requestLayout();
+    }
+
+    public void setTextColor(int color) {
+        mTextPaint.setColor(color);
+        requestLayout();
+    }
+
+    public void setLightAngle(float degree) {
+        mShadowOffsetX = (int) Math.round(mShadowSize*Math.cos(Math.toDegrees(degree)));
+        mShadowOffsetY = (int) Math.round(mShadowSize*Math.sin(Math.toDegrees(degree)));
+        invalidate();
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        final int width = MeasureSpec.getSize(widthMeasureSpec);
+        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int height;
+
+        mTextLayout = new StaticLayout(mText, mTextPaint,
+                width - mTextPaddingLeft - mTextPaddingRight - 2 * mShadowSize,
+                Layout.Alignment.ALIGN_CENTER, 1.0f, 0f, true);
+
+        mBodyBounds.right = getWidth() - 2 * mShadowSize;
+        mBodyBounds.bottom = mTextLayout.getHeight() + mTextPaddingTop + mTextPaddingBottom;
+
+        if(heightMode == MeasureSpec.EXACTLY)
+            height = heightSize;
+        else {
+            height = Math.round(mBodyBounds.height() + mBodyBounds.width()*mPinOffsetY + 2*mShadowSize);
+            height = Math.min(height, heightSize);
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBodyPath = null;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mBodyPath == null) {
+            mBodyPath = new Path();
+            mBodyPath.setFillType(Path.FillType.EVEN_ODD);
+            mBodyPath.addRoundRect(mBodyBounds, mCornerRadius, mCornerRadius, Path.Direction.CW);
+            mBodyPath.addCircle(2 * mCornerRadius, 2 * mCornerRadius, mCornerRadius, Path.Direction.CW);
+            mBodyPath.addCircle(mBodyBounds.width() - 2 * mCornerRadius, 2 * mCornerRadius, mCornerRadius, Path.Direction.CW);
+        }
+
+        canvas.save();
+        canvas.translate(mShadowSize, mShadowSize);
+        canvas.translate(0, mBodyBounds.width()*mPinOffsetY);
+
+        // Draw strings
+        final float pinX = mPinOffsetX*mBodyBounds.width();
+        final float pinY = mPinRadius - mPinOffsetY*mBodyBounds.width();
+        canvas.drawLine(mCornerRadius, mCornerRadius, pinX, pinY - 2*mStringsPaint.getStrokeWidth(), mStringsPaint);
+        canvas.drawLine(mBodyBounds.width() - mCornerRadius, mCornerRadius, pinX, pinY - 2*mStringsPaint.getStrokeWidth(), mStringsPaint);
+
+        // Draw shadow
+        if(mShadowSize != 0) {
+            canvas.translate(mShadowOffsetX, mShadowOffsetY);
+            canvas.drawPath(mBodyPath, mSignShadowPaint);
+            canvas.translate(-mShadowOffsetX, -mShadowOffsetY);
+        }
+
+        // Draw pin
+        canvas.drawCircle(pinX + mShadowOffsetX, pinY + mShadowOffsetY, mPinRadius, mPinShadowPaint);
+        canvas.drawCircle(pinX, pinY, mPinRadius, mPinPaint);
+
+        // Draw body
+        canvas.drawPath(mBodyPath, mSignPaint);
+
+        // Draw text
+        canvas.translate(mTextPaddingLeft, mTextPaddingTop + (mBodyBounds.height() - mTextLayout.getHeight() - mTextPaddingTop - mTextPaddingBottom)/2);
+        mTextLayout.draw(canvas);
+
+        canvas.restore();
+    }
+
+    static private int darkerOf(int color, float ratio) {
+        float[] hsl = new float[3];
+        ColorUtils.colorToHSL(color, hsl);
+        hsl[2]*=ratio;
+        return ColorUtils.HSLToColor(hsl);
+    }
+
+    private static class Options {
+
+        CharSequence text;
+        int cornerRadius;
+        int shadowSize;
+        int textSize;
+        Typeface typeFace;
+        int textColor;
+        int color;
+        int pinRadius;
+        int pinColor;
+        int stringsSize;
+        int stringsColor;
+        int textPaddingLeft;
+        int textPaddingTop;
+        int textPaddingRight;
+        int textPaddingBottom;
+        float pinOffsetX = 0.5f;
+        float pinOffsetY = 0.2f;
+
+        static Options from(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+            final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DoorSignView, defStyleAttr, defStyleRes);
+            final Options options = new Options();
+
+            options.text = a.getText(R.styleable.DoorSignView_android_text);
+            options.cornerRadius = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_cornerRadius, 0);
+            options.shadowSize = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_shadowSize, 0);
+            options.textSize = a.getDimensionPixelSize(R.styleable.DoorSignView_android_textSize, 20);
+            options.textColor = a.getColor(R.styleable.DoorSignView_android_textColor, Color.BLACK);
+            options.color = a.getColor(R.styleable.DoorSignView_dsv_signColor, Color.DKGRAY);
+            options.pinRadius = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_pinRadius, 20);
+            options.pinColor = a.getColor(R.styleable.DoorSignView_dsv_pinColor, Color.WHITE);
+            options.stringsSize = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_stringsWidth, 10);
+            options.stringsColor= a.getColor(R.styleable.DoorSignView_dsv_stringsColor, Color.LTGRAY);
+            options.textPaddingLeft = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_textPaddingLeft, 0);
+            options.textPaddingTop = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_textPaddingTop, 0);
+            options.textPaddingRight = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_textPaddingRight, 0);
+            options.textPaddingBottom = a.getDimensionPixelSize(R.styleable.DoorSignView_dsv_textPaddingBottom, 0);
+            options.pinOffsetX = a.getFloat(R.styleable.DoorSignView_dsv_pinOffsetX, 0.5f);
+            options.pinOffsetY = a.getFloat(R.styleable.DoorSignView_dsv_pinOffsetY, 0.25f);
+
+            final String textFont = a.getString(R.styleable.DoorSignView_dsv_textFont);
+            if(textFont != null)
+                options.typeFace = Typeface.createFromAsset(context.getAssets(), textFont);
+            else switch(a.getInt(R.styleable.DoorSignView_android_typeface, 0)) {
+                case 0:
+                    options.typeFace = Typeface.DEFAULT;
+                    break;
+                case 1:
+                    options.typeFace = Typeface.SANS_SERIF;
+                    break;
+                case 2:
+                    options.typeFace = Typeface.SERIF;
+                    break;
+                case 3:
+                    options.typeFace = Typeface.MONOSPACE;
+                    break;
+            }
+
+            a.recycle();
+
+            return options;
+        }
+    }
+}
---
+++ sample/src/main/java/com/mypopsy/sample/doorsignview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mypopsy.sample.doorsignview;
+
+import android.os.Debug;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (BuildConfig.DEBUG) {
+            if (Debug.isDebuggerConnected()) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        }
+    }
+}
