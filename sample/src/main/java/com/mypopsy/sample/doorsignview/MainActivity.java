c0cb2a457923d22d04c7a3d516c8fad3cdbb577b
Cerrato Renaud <renaud.cerrato@gmail.com>
2016-09-12 09:01:36 -0400

    added seekbar for rotation

--- sample/src/main/java/com/mypopsy/sample/doorsignview/MainActivity.java
+++ sample/src/main/java/com/mypopsy/sample/doorsignview/MainActivity.java
@@ -1,17 +1,44 @@
 package com.mypopsy.sample.doorsignview;
 
 import android.os.Debug;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar mSeekBar;
+    private View mDoorSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSeekBar = (SeekBar) findViewById(R.id.seekbar);
+        mDoorSign = findViewById(R.id.image);
+        mSeekBar.setProgress(180 + (int) mDoorSign.getRotation());
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int value, boolean isUser) {
+                if(isUser) {
+                    mDoorSign.setRotation(value - 180);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
